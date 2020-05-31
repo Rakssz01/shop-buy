bb42bb16b46f170a133432965edc22c79bc6fef3
Rakssz01 <esaalam55@gmail.com>
2020-06-01 01:21:30 +0600

    added styles and broken for homepage

---
+++ src/components/menu-item/Menu-item.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./menu-item.scss"
+
+const Menuitem = ({title,image,size}) => {
+  return (
+    <div className={`${size} menu-item`}>
+        <div className='background-image' style={{
+        backgroundImage:`url(${image})`
+    }} />
+      <div className="content">
+        <div className="title">{title.toUpperCase()}</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default Menuitem;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './homepage.scss'
+import Directory from '../components/directory/Directory';
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+    <h1 className='title'>Welcome to my Homepage</h1>
+    <Directory />
+    
+  </div>
+);
+    
+}
+
+export default Homepage
